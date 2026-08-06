3aa7176e66502b22a9e587a7f373359092facc0f
yuto <99kyy5zn46@privaterelay.appleid.com>
2026-08-06 22:37:07 +0900

    feat(tpil): add chapter 3 notes on logical connectives

--- lean/Math/Learn/TPiL/C03PropositionsAndProofs.lean
+++ lean/Math/Learn/TPiL/C03PropositionsAndProofs.lean
@@ -89,3 +89,67 @@ theorem t6 (h₁ : q → r) (h₂ : p → q) : p → r :=
   show r from h₁ (h₂ h₃)
 
 end s2
+
+namespace s3
+
+variable (p q r : Prop)
+
+example (hp : p) (hq : q) : p ∧ q := And.intro hp hq
+
+#check fun (hp : p) (hq : q) => And.intro hp hq
+
+/- and -/
+
+example (h : p ∧ q) : p := And.left h
+
+example (h : p ∧ q) : q ∧ p := And.intro (And.right h) (And.left h)
+
+variable (xs : List Nat)
+#check List.length xs
+#check xs.length
+
+example (h : p ∧ q) : q ∧ p := ⟨h.right, h.left⟩
+
+/- or -/
+
+example (hp : p) : p ∨ q := Or.intro_left q hp
+
+example (h : p ∨ q) : q ∨ p :=
+  Or.elim h
+    (fun hp : p =>
+      show q ∨ p from Or.intro_right q hp)
+    (fun hq : q =>
+      show q ∨ p from Or.intro_left p hq)
+
+example (h : p ∨ q) : q ∨ p :=
+  Or.elim h (fun hp => Or.inr hp) (fun hq => Or.inl hq)
+
+example (h : p ∨ q) : q ∨ p :=
+  h.elim (fun hp => Or.inr hp) (fun hq => Or.inl hq)
+
+/- not -/
+
+example (hpq : p → q) (hnq : ¬q) : ¬p :=
+  fun hp : p =>
+  show False from hnq (hpq hp)
+
+example (hp : p) (hnp : ¬p) : q := absurd hp hnp
+
+/- equivalence -/
+
+theorem and_swap : p ∧ q ↔ q ∧ p :=
+  Iff.intro
+    (fun h : p ∧ q => show q ∧ p from And.intro (And.right h) (And.left h))
+    (fun h : q ∧ p => show p ∧ q from And.intro (And.right h) (And.left h))
+
+#check and_swap p q
+#check Iff.mp (and_swap p q)
+variable (h : p ∧ q)
+example : q ∧ p := Iff.mp (and_swap p q ) h
+
+theorem and_swap' : p ∧ q ↔ q ∧ p :=
+  ⟨ fun h => ⟨h.right, h.left⟩, fun h => ⟨h.right, h.left⟩⟩
+
+example (h : p ∧ q) : q ∧ p := (and_swap p q).mp h
+
+end s3
