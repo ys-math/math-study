a4ed32b90176038f58885f2a7edccf2113f46ac4
yuto <99kyy5zn46@privaterelay.appleid.com>
2026-08-12 21:04:37 +1000

    feat(lean): add TPiL 4.2 substitution and congruence examples

--- lean/Math/Learn/TPiL/C04QuantifiersAndEquality.lean
+++ lean/Math/Learn/TPiL/C04QuantifiersAndEquality.lean
@@ -66,4 +66,20 @@ example (f : α → β) (a : α) : (fun x => f x) a = f a := Eq.refl _
 example (a : α) (b : β) : (a, b).1 = a := rfl
 example : 2 + 3 = 5 := Eq.refl _
 
+example (α : Type) (a b : α) (p : α → Prop) (h1 : a = b) (h2 : p a) : p b :=
+  Eq.subst h1 h2
+
+example (α : Type) (a b : α) (p : α → Prop) (h1 : a = b) (h2 : p a) : p b :=
+  h1 ▸ h2
+
+variable (α : Type)
+variable (a b : α)
+variable (f g : α → Nat)
+variable (h₁ : a = b)
+variable (h₂ : f = g)
+
+example : f a = f b := congrArg f h₁
+example : f a = g a := congrFun h₂ a
+example : f a = g b := congr h₂ h₁
+
 end s2
