efe39c601e84dfaf84a5f3b3f5c8efd13baf90d1
yuto <99kyy5zn46@privaterelay.appleid.com>
2026-08-18 21:12:11 +1000

    feat(lean): add TPiL 5.2 basic tactic examples

--- lean/Math/Learn/TPiL/C05Tactics.lean
+++ lean/Math/Learn/TPiL/C05Tactics.lean
@@ -48,3 +48,99 @@ theorem test'''' (p q : Prop) (hp : p) (hq : q) : p ∧ q ∧ p := by
     · exact hp
 
 end s1
+
+namespace s2
+
+example (p q r : Prop) : p ∧ (q ∨ r) ↔ (p ∧ q) ∨ (p ∧ r) := by
+  apply Iff.intro
+  · intro h
+    apply Or.elim (And.right h)
+    · intro hq
+      apply Or.inl
+      apply And.intro
+      · exact And.left h
+      · exact hq
+    · intro hr
+      apply Or.inr
+      apply And.intro
+      · exact And.left h
+      · exact hr
+  · intro h
+    apply Or.elim h
+    · intro hpq
+      apply And.intro
+      · exact And.left hpq
+      · apply Or.inl
+        exact And.right hpq
+    · intro hpr
+      apply And.intro
+      · exact And.left hpr
+      · apply Or.inr
+        exact And.right hpr
+
+example (α : Type) : α → α := by
+  intro a
+  exact a
+
+example (α : Type) : ∀ x : α, x = x := by
+  intro x
+  exact Eq.refl x
+
+example (p q : α → Prop) : (∃ x, p x ∧ q x) → ∃ x, q x ∧ p x := by
+  intro ⟨w, hpw, hqw⟩
+  exact ⟨w, hqw, hpw⟩
+
+example (p q : α → Prop) : (∃ x, p x ∨ q x) → ∃ x, q x ∨ p x := by
+  intro
+  | ⟨w, Or.inl h⟩ => exact ⟨w, Or.inr h⟩
+  | ⟨w, Or.inr h⟩ => exact ⟨w, Or.inl h⟩
+
+variable (x y z w : Nat)
+
+example (h₁ : x = y) (h₂ : y = z) (h₃ : z = w) : x = w := by
+  apply Eq.trans h₁
+  apply Eq.trans h₂
+  assumption   -- applied h₃
+
+example : ∀ a b c : Nat, a = b → a = c → c = b := by
+  intros
+  apply Eq.trans
+  apply Eq.symm
+  assumption
+  assumption
+
+example : ∀ a b c : Nat, a = b → a = c → c = b := by unhygienic
+  intros
+  apply Eq.trans
+  apply Eq.symm
+  exact a_2
+  exact a_1
+
+example : ∀ a b c : Nat, a = b → a = c → c = b := by
+  intros
+  apply Eq.trans
+  apply Eq.symm
+  repeat assumption
+
+example (x : Nat) : x = x := by
+  revert x
+  intro y
+  rfl
+
+example (x y : Nat) (h : x = y) : y = x := by
+  revert x y
+  intros
+  apply Eq.symm
+  assumption
+
+example : 3 = 3 := by
+  generalize 3 = x
+  revert x
+  intro y
+  rfl
+
+example : 2 + 3 = 5 := by
+  generalize h : 3 = x
+  rw [← h]
+
+end s2
