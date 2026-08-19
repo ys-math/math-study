b9b9d26f4de48b2bdd11063a0184aa25bfe90825
yuto <99kyy5zn46@privaterelay.appleid.com>
2026-08-19 21:09:55 +1000

    feat(lean): add TPiL cases and constructor tactic examples

--- lean/Math/Learn/TPiL/C05Tactics.lean
+++ lean/Math/Learn/TPiL/C05Tactics.lean
@@ -144,3 +144,118 @@ example : 2 + 3 = 5 := by
   rw [← h]
 
 end s2
+
+namespace s3
+
+example (p q : Prop) : p ∨ q → q ∨ p := by
+  intro h
+  cases h with
+  | inl hp => apply Or.inr ; exact hp
+  | inr hq => apply Or.inl ; exact hq
+
+example (p q : Prop) : p ∨ q → q ∨ p := by
+  intro h
+  cases h
+  apply Or.inr
+  assumption
+  apply Or.inl
+  assumption
+
+example (p : Prop) : p ∨ p → p := by
+  intro h
+  cases h
+  repeat assumption
+
+example (p : Prop) : p ∨ p → p := by
+  intro h
+  cases h <;> assumption
+
+example (p q : Prop) : p ∨ q → q ∨ p := by
+  intro h
+  cases h
+  · apply Or.inr
+    assumption
+  · apply Or.inl
+    assumption
+
+example (p q : Prop) : p ∨ q → q ∨ p := by
+  intro h
+  cases h
+  case inr h =>
+    apply Or.inl
+    assumption
+  case inl h =>
+    apply Or.inr
+    assumption
+
+example (p q : Prop) : p ∨ q → q ∨ p := by
+  intro h
+  cases h
+  case inr h =>
+    apply Or.inl
+    assumption
+  · apply Or.inr
+    assumption
+
+example (p q : Prop) : p ∧ q → q ∧ p := by
+  intro h
+  cases h with
+  | intro hp hq => constructor ; exact hq ; exact hp
+
+example (p q r : Prop) : p ∧ (q ∨ r) ↔ (p ∧ q) ∨ (p ∧ r) := by
+  apply Iff.intro
+  · intro h
+    cases h with
+    | intro hp hqr =>
+      cases hqr
+      · apply Or.inl ; constructor <;> assumption
+      · apply Or.inr ; constructor <;> assumption
+  · intro h
+    cases h with
+    | inl hpq =>
+      cases hpq with
+      | intro hp hq =>
+        constructor ; exact hp ; apply Or.inl ; exact hq
+    | inr hpr =>
+      cases hpr with
+      | intro hp hr =>
+        constructor ; exact hp ; apply Or.inr ; exact hr
+
+example (p q : Nat → Prop) : (∃ x, p x) → ∃ x, p x ∨ q x := by
+  intro h
+  cases h with
+    | intro x px => constructor ; apply Or.inl ; exact px
+
+example (p q : Nat → Prop) : (∃ x, p x) → ∃ x, p x ∨ q x := by
+  intro h
+  cases h with
+  | intro x px => exists x ; apply Or.inl ; exact px
+
+example (p q : Nat → Prop) : (∃ x, p x ∧ q x) → ∃ x, q x ∧ p x := by
+  intro h
+  cases h with
+  | intro x hpq =>
+    cases hpq with
+    | intro hp hq =>
+      exists x
+
+def swap_pair : α × β → β × α := by
+  intro p
+  cases p
+  constructor <;> assumption
+
+def swap_sum : Sum α β → Sum β α := by
+  intro p
+  cases p
+  · apply Sum.inr ; assumption
+  · apply Sum.inl ; assumption
+
+open Nat
+example (P : Nat → Prop)
+    (h₀ : P 0) (h₁ : ∀ n, P (succ n))
+    (m : Nat) : P m := by
+  cases m with
+  | zero => exact h₀
+  | succ m' => exact h₁ m'
+
+end s3
