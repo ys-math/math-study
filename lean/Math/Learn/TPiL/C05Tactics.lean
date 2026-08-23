0394e385b586fe25e3def3635aaabe0e3e05b1e7
yuto <99kyy5zn46@privaterelay.appleid.com>
2026-08-23 10:40:25 +1000

    feat(lean): add TPiL rewrite tactic examples

--- lean/Math/Learn/TPiL/C05Tactics.lean
+++ lean/Math/Learn/TPiL/C05Tactics.lean
@@ -410,3 +410,30 @@ example (p q r : Prop) (hp : p) (hq : q) (hr : r) :
   repeat (any_goals (first | constructor | assumption))
 
 end s5
+
+namespace s6
+
+variable (k : Nat) (f : Nat → Nat)
+  example (h₁ : f 0 = 0) (h₂ : k = 0) : f k = 0 := by
+    rw [h₂]
+    rw [h₁]
+
+example (x y : Nat) (p : Nat → Prop) (q : Prop) (h : q → x = y) (h' : p y) (hq : q) : p x := by
+  rw [h hq] ; assumption
+
+variable (a b : Nat) (f : Nat → Nat)
+example (h₁ : a = b) (h₂ : f a = 0) : f b = 0 := by
+  rw [←h₁, h₂]
+
+example (f : Nat → Nat) (a : Nat) (h : a + 0 = 0) : f a = f 0 := by
+  rw [Nat.add_zero] at h
+  rw [h]
+
+def Tuple (α : Type) (n : Nat) :=
+  { as : List α // as.length = n}
+
+example {α} (n : Nat) (h : n = 0) (t : Tuple α n) : Tuple α 0 := by
+  rw [h] at t
+  exact t
+
+end s6
