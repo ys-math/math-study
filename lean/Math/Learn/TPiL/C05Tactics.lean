f5fad95f7776bb6c6861bf01994ec17d18603afa
yuto <99kyy5zn46@privaterelay.appleid.com>
2026-08-24 22:33:09 +1000

    feat(lean): add TPiL split tactic examples

--- lean/Math/Learn/TPiL/C05Tactics.lean
+++ lean/Math/Learn/TPiL/C05Tactics.lean
@@ -499,3 +499,32 @@ example (xs ys : List Nat) (p : List Nat → Prop) (h : p (xs ++ mk_symm ys).rev
   simp [reverse_mk_symm] at h ; assumption
 
 end s7
+
+namespace c05s8
+
+def f (x y z : Nat) : Nat :=
+  match x, y, z with
+  | 5, _, _ => y
+  | _, 5, _ => y
+  | _, _, 5 => y
+  | _, _, _ => 1
+
+example (x y z : Nat) : x ≠ 5 → y ≠ 5 → z ≠ 5 → z = w → f x y w = 1 := by
+  intros
+  simp [f]
+  split
+  · contradiction
+  · contradiction
+  · contradiction
+  · rfl
+
+def g (xs ys : List Nat) : Nat :=
+  match xs, ys with
+  | [a, b], _ => a+b+1
+  | _, [b, _] => b+1
+  | _, _      => 1
+
+example (xs ys : List Nat) (h : g xs ys = 0) : False := by
+  simp [g] at h; split at h <;> simp +arith at h
+
+end c05s8
